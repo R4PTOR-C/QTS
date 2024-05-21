55c5fa82783d0576382b1940dc3522fcb67c99e5
rafael.caldas <rafaelcroriz7@gmail.com>
2024-05-21 11:00:02 -0300

    .

--- app.py
+++ app.py
@@ -15,6 +15,16 @@ def connect_db():
         port='5432'
     )
 
+def quicksort(arr, key=lambda x: x):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if key(x) < key(pivot)]
+    middle = [x for x in arr if key(x) == key(pivot)]
+    right = [x for x in arr if key(x) > key(pivot)]
+    return quicksort(left, key) + middle + quicksort(right, key)
+
+
 @app.route('/')
 def index():
     conn = connect_db()
@@ -43,14 +53,19 @@ def new_professor():
     return render_template('new.html')
 
 @app.route('/disciplinas')
-def indexDisciplinas():
+def index_disciplinas():
     conn = connect_db()
     cur = conn.cursor()
     cur.execute('SELECT * FROM disciplinas')
     disciplinas = cur.fetchall()
     cur.close()
     conn.close()
+
+    # Ordenar os dados pela carga horária (segunda coluna, índice 1)
+    disciplinas = quicksort(disciplinas, key=lambda x: x[2])
+
     return render_template('disciplinas/indexDisciplina.html', disciplinas=disciplinas)
+
 
 @app.route('/new_disciplina', methods=['GET', 'POST'])
 def new_disciplina():
@@ -64,20 +79,38 @@ def new_disciplina():
             conn.commit()
             cur.close()
             conn.close()
-            return redirect(url_for('indexDisciplinas'))
+            return redirect(url_for('index_disciplinas'))
         except Exception as e:
             print(f"Ocorreu um erro ao inserir o professor: {e}")
     return render_template('disciplinas/newDisciplina.html')
 
 @app.route('/cursos')
-def indexCurso():
+def index_curso():
     conn = connect_db()
     cur = conn.cursor()
     cur.execute('SELECT * FROM cursos')
     cursos = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('cursos/indexCursos.html', cursos=cursos)
+    return render_template('cursos/indexCurso.html', cursos=cursos)
+
+@app.route('/new_curso', methods=['GET', 'POST'])
+def new_curso():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        duracao = request.form['duracao']
+        disciplina = request.form['disciplina']
+        try:
+            conn = connect_db()
+            cur = conn.cursor()
+            cur.execute('INSERT INTO cursos (nome, duracao, disciplina) VALUES (%s, %s, %s)', (nome, duracao, disciplina))
+            conn.commit()
+            cur.close()
+            conn.close()
+            return redirect(url_for('index_curso'))
+        except Exception as e:
+            print(f"Ocorreu um erro ao inserir o professor: {e}")
+    return render_template('cursos/newCurso.html')
 # Adicione outras rotas e handlers conforme necessário
 
 if __name__ == '__main__':
